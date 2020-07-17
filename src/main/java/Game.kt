fd38c4c4d03f16a33461e7b56b1a25a629f3fcea
Justin Man <justin.man@moonpig.com>
2020-07-17 10:19:29 +0100

    Use structural jump expressions to reduce nested ifs

--- src/main/java/Game.kt
+++ src/main/java/Game.kt
@@ -13,38 +13,37 @@ class Game {
         var startNewFrame = true
         var finalFrameRolls = 0
         var frameIndex = 0
-        rolls.forEachIndexed { index, roll ->
+        for (index in 0 until rolls.size) {
             if (startNewFrame) frameIndex++
 
-            runningScore += roll
+            runningScore += rolls[index]
             if (frameIndex != 10) {
-                val isSpare = !startNewFrame && (roll + rolls[index - 1] == 10)
-                val isStrike = startNewFrame && roll == 10
-                if (isSpare) {
-                    if (index < rolls.lastIndex) {
-                        runningScore += rolls[index + 1]
-                    }
-                }
-                if (isStrike) {
-                    if (index < rolls.lastIndex) {
-                        runningScore += rolls[index + 1]
-
-                    }
-                    if (index + 1 < rolls.lastIndex) {
-                        runningScore += rolls[index + 2]
-                    }
-                    startNewFrame = true
-                } else {
+                val isSpare = !startNewFrame && (rolls[index] + rolls[index - 1] == 10)
+                val isStrike = startNewFrame && rolls[index] == 10
+                if (!isStrike && !isSpare) {
                     startNewFrame = !startNewFrame
+                    continue
                 }
+                val bonusRollIndex = index + 1
+                val strikeOnlyBonusIndex = index + 2
+
+                runningScore += rolls.getOrNull(bonusRollIndex) ?: 0
+
+                if (isStrike) {
+                    runningScore += rolls.getOrNull(strikeOnlyBonusIndex) ?: 0
+                }
+
+                startNewFrame = true
+
             } else {
                 startNewFrame = false
                 finalFrameRolls++
 
                 if (finalFrameRolls == 3) isGameOver = true
-                else if (finalFrameRolls == 2 && (roll + rolls[index - 1] < 10)) isGameOver = true
+                else if (finalFrameRolls == 2 && (rolls[index] + rolls[index - 1] < 10)) isGameOver = true
             }
         }
+
         score = Score(runningScore)
     }
 }
